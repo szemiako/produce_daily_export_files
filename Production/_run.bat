01d9a67889305cb9c53d4175ec2dcff86b3d5f4b
Daniel <szemiako@users.noreply.github.com>
2021-04-16 13:43:25 -0400

    Batch to link all PS scripts together
    Didn't want to create a monolithic structure with the py scripts. The py scripts sole function is to produce export files. The PS scripts move the files for delivery.

---
+++ Production/_run.bat
@@ -0,0 +1,3 @@
+Powershell.exe -executionpolicy Unrestricted -File "C:\Production\_wrapper.ps1"
+Powershell.exe -executionpolicy Unrestricted -File "C:\Production\_encrypt.ps1"
+Powershell.exe -executionpolicy Unrestricted -File "C:\Production\_push.ps1"
